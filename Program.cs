dc2f386078c63fd8f9a392ebbb1647229dfebb65
Mikhail Kuzmin <mikhalkuzmin@yandex.ru>
2022-09-25 16:40:49 +0400

    Task1 and Task2 added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+void Task1()
+{
+    string str = Console.ReadLine().ToString();
+    Console.WriteLine(str[1]);
+}
+
+void Task2()
+{
+    int a = int.Parse(Console.ReadLine());
+    if(a > 99)
+    {
+        string str = a.ToString();
+        Console.WriteLine(str[2]);
+    }
+    else
+    {
+        Console.WriteLine(" Несодержит третьего числа");
+    }
+}
+
+void Task3()
+{
+    
+}
+
+while (true)
+{
+    int a= int.Parse(Console.ReadLine());
+    if( a == 1)
+    {
+        Task1();
+    }
+    if( a == 2)
+    {
+        Task2();
+    }
+    if( a == 3)
+    {
+        Task3();
+    }
+}
